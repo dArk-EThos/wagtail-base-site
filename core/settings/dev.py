51cb8a94cb9e50ae5b043f466ddb0ff3e69d3208
dArk-EThos <lmaccoy@hotmail.com>
2020-12-17 11:41:34 -0600

    Base settings for Wagtail Site

---
+++ core/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
